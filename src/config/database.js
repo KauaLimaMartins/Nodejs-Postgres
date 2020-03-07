9f89b724752cd9d657826b963ef4a80e847d1daa
KauaLimaMartins <kaualimadev0601@gmail.com>
2020-03-07 19:27:42 -0300

    Settings

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '',
+  username: '',
+  password: '',
+  database: '',
+
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+  }
+};
