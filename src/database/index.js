7e9ca69d77596781f30e41139afeb0c8cf94aaee
KauãDev <kaualimadev0601@gmail.com>
2020-03-09 09:53:10 -0300

    Update index.js

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+const dbConfig = require('../config/database');
+
+const models = [];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    this.connection = new Sequelize(dbConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+module.exports = new Database();
